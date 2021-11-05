2e21ceb6adaf899281a09a9b7b013eb20b8391c9
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2021-11-05 18:44:15 +0530

    django complete

--- GAN/forms.py
+++ GAN/forms.py
@@ -2,4 +2,4 @@ from typing import Text
 from django import forms
 
 class Textform(forms.Form):
-    Text = forms.CharField(label='description', max_length=100)
+    Text = forms.CharField(label='description', max_length=100,widget=forms.TextInput(attrs={'size':30}))
--- StyleGAN/settings.py
+++ StyleGAN/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static"]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
 MEDIA_URL = '/media/images/'
